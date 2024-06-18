e5f516181ba6f05d9ec8a6ffc8ce098918c010c4
Борис Кебаб <levon44432@gmail.com>
2024-06-18 15:40:57 +0300

    Added DI extensions to application and infrastructure layers

---
+++ TestPryaniky.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TestPryaniky.Core.Repositories;
+using TestPryaniky.Infrastructure.Interceptors;
+using TestPryaniky.Infrastructure.Repositories;
+
+namespace TestPryaniky.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connection = configuration.GetConnectionString("Database");
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+        {
+            options.AddInterceptors(serviceProvider.GetServices<ISaveChangesInterceptor>());
+            options.UseNpgsql(connection);
+        });
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        
+        return services;
+    }
+}
